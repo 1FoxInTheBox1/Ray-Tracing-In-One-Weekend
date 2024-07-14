f2efd4c2f306feb93685735bdb85ab10926f3ac0
rhit-yangnj <yangnj@rose-hulman.edu>
2024-07-13 21:50:45 -0500

    Camera class

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "sphere.h"
+#include "camera.h"
 #include "hitable_list.h"
 #include <fstream>
 using namespace std;
@@ -23,22 +24,18 @@ vec3 color(const ray &r, hitable *world)
 
 int main()
 {
-    int nx = 600;
-    int ny = 300;
+    int nx = 200;
+    int ny = 100;
     ofstream myfile;
     myfile.open("out.ppm");
     myfile << "P3\n"
            << nx << " " << ny << "\n255\n";
 
-    vec3 lower_left_corner(-2.0, -1.0, -1.0);
-    vec3 horizontal(4.0, 0.0, 0.0);
-    vec3 vertical(0.0, 2.0, 0.0);
-    vec3 origin(0.0, 0.0, 0.0);
-
     hitable *list[2];
     list[0] = new sphere(vec3(0, 0, -1), 0.5);
     list[1] = new sphere(vec3(0, -100.5, -1), 100);
     hitable *world = new hitable_list(list, 2);
+    camera cam;
     
     for (int j = ny - 1; j >= 0; j--)
     {
@@ -46,7 +43,8 @@ int main()
         {
             float u = float(i) / float(nx);
             float v = float(j) / float(ny);
-            ray r(origin, lower_left_corner + u * horizontal + v * vertical);
+            ray r = cam.get_ray(u, v);
+            
             
             vec3 p = r.point_at_parameter(2.0);
             vec3 col = color(r, world);
