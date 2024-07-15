03b8145b587349f3851ec06b680b890120cf7fb8
rhit-yangnj <yangnj@rose-hulman.edu>
2024-07-15 12:49:04 -0500

    Chapter 9 Complete

--- camera.h
+++ camera.h
@@ -12,6 +12,7 @@ public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int image_width = 100;      // Rendered image width in pixel count
     int samples_per_pixel = 10; // Count of random samples for each pixel
+    int max_depth = 10;          // Maximum number of ray bounces
 
     void render(const hittable &world)
     {
@@ -30,9 +31,10 @@ public:
             for (int i = 0; i < image_width; i++)
             {
                 color pixel_color(0, 0, 0);
-                for (int sample = 0; sample < samples_per_pixel; sample++) {
+                for (int sample = 0; sample < samples_per_pixel; sample++)
+                {
                     ray r = get_ray(i, j);
-                    pixel_color += ray_color(r, world);
+                    pixel_color += ray_color(r, max_depth, world);
                 }
                 write_color(output_file, pixel_samples_scale * pixel_color);
             }
@@ -76,14 +78,13 @@ private:
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
-    ray get_ray(int i, int j) const {
+    ray get_ray(int i, int j) const
+    {
         // Construct a camera ray originating from the origin and directed at randomly sampled
         // point around the pixel location i, j
 
         auto offset = sample_square();
-        auto pixel_sample = pixel00_loc
-        + ((i + offset.x()) * pixel_delta_u)
-        + ((j + offset.y()) * pixel_delta_v);
+        auto pixel_sample = pixel00_loc + ((i + offset.x()) * pixel_delta_u) + ((j + offset.y()) * pixel_delta_v);
 
         auto ray_origin = center;
         auto ray_direction = pixel_sample - ray_origin;
@@ -91,17 +92,24 @@ private:
         return ray(ray_origin, ray_direction);
     }
 
-    vec3 sample_square() const {
+    vec3 sample_square() const
+    {
         // Returns the vector to a random point in the [-.5, -.5]-[+.5,+.5] unit square
         return vec3(random_double() - 0.5, random_double() - 0.5, 0);
     }
 
-    color ray_color(const ray &r, const hittable &world) const
+    color ray_color(const ray &r, int depth, const hittable &world) const
     {
+        // If we've exceeded the ray bounce limit, return black
+        if (depth <= 0) {
+            return color(0, 0, 0);
+        }
+
         hit_record rec;
-        if (world.hit(r, interval(0, infinity), rec))
+        if (world.hit(r, interval(0.001, infinity), rec))
         {
-            return 0.5 * (rec.normal + color(1, 1, 1));
+            vec3 bounce_direction = rec.normal + random_unit_vector();
+            return 0.5 * ray_color(ray(rec.p, bounce_direction), depth-1, world);
         }
 
         // Create background gradient with a linear interpolation
