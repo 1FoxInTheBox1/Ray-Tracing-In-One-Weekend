88f2514805872cfa3822e34393b7dac0762e20e8
rhit-yangnj <yangnj@rose-hulman.edu>
2024-07-15 12:18:04 -0500

    Chapter 8 Complete

--- rtweekend.h
+++ rtweekend.h
@@ -20,6 +20,16 @@ inline double degrees_to_radian(double degrees) {
     return degrees * pi / 180;
 }
 
+inline double random_double() {
+    // Return a random real in [0, 1)
+    return rand() / (RAND_MAX + 1.0);
+}
+
+inline double random_double(double min, double max) {
+    // Returns a random real in [min, max)
+    return min + (max-min)*random_double();
+}
+
 // Common Headers
 #include "color.h"
 #include "interval.h"
