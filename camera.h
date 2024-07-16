4e30619e81df46e91929c9bebae6e3f013bb7c2e
rhit-yangnj <yangnj@rose-hulman.edu>
2024-07-16 10:26:00 -0500

    Chapter 12 Complete

--- camera.h
+++ camera.h
@@ -13,7 +13,12 @@ public:
     double aspect_ratio = 1.0;  // Ratio of image width over height
     int image_width = 100;      // Rendered image width in pixel count
     int samples_per_pixel = 10; // Count of random samples for each pixel
-    int max_depth = 10;          // Maximum number of ray bounces
+    int max_depth = 10;         // Maximum number of ray bounces
+
+    double vfov = 90;                  // Vertical view angle (field of view)
+    point3 lookfrom = point3(0, 0, 0); // Point camera is looking from
+    point3 lookat = point3(0, 0, -1);  // Point camera is looking at
+    vec3 vup = vec3(0, 1, 0);          // Camera-relative "up" direction
 
     void render(const hittable &world)
     {
@@ -51,6 +56,7 @@ private:
     point3 pixel00_loc;         // Location of pixel 0, 0
     vec3 pixel_delta_u;         // Offset to pixel to the right
     vec3 pixel_delta_v;         // Offset to pixel below
+    vec3 u, v, w;               // Camera frame basis vectors
 
     void initialize()
     {
@@ -59,23 +65,30 @@ private:
 
         pixel_samples_scale = 1.0 / samples_per_pixel;
 
-        center = point3(0, 0, 0);
+        center = lookfrom;
 
         // Viewport Dimensions
-        auto focal_length = 1.0;
-        auto viewport_height = 2.0;
+        auto focal_length = (lookfrom - lookat).length();
+        auto theta = degrees_to_radian(vfov);
+        auto h = tan(theta / 2);
+        auto viewport_height = 2 * h * focal_length;
         auto viewport_width = viewport_height * (double(image_width) / image_height);
 
+        // Calculate unit basis vectors
+        w = unit_vector(lookfrom - lookat);
+        u = unit_vector(cross(vup, w));
+        v = cross(w, u);
+
         // Calculate Viewport Vectors
-        auto viewport_u = vec3(viewport_width, 0, 0);
-        auto viewport_v = vec3(0, -viewport_height, 0);
+        auto viewport_u = viewport_width * u;
+        auto viewport_v = viewport_height * -v;
 
         // Calculate horizontal and vertical delta vectors from pixel to pixel
         pixel_delta_u = viewport_u / image_width;
         pixel_delta_v = viewport_v / image_height;
 
         // Calculate the location of the upper left pixel
-        auto viewport_upper_left = center - vec3(0, 0, focal_length) - viewport_u / 2 - viewport_v / 2;
+        auto viewport_upper_left = center - (focal_length * w) - viewport_u / 2 - viewport_v / 2;
         pixel00_loc = viewport_upper_left + 0.5 * (pixel_delta_u + pixel_delta_v);
     }
 
@@ -102,7 +115,8 @@ private:
     color ray_color(const ray &r, int depth, const hittable &world) const
     {
         // If we've exceeded the ray bounce limit, return black
-        if (depth <= 0) {
+        if (depth <= 0)
+        {
             return color(0, 0, 0);
         }
 
@@ -112,7 +126,7 @@ private:
             ray scattered;
             color attenuation;
             if (rec.mat->scatter(r, rec, attenuation, scattered))
-                return attenuation * ray_color(scattered, depth-1, world);
+                return attenuation * ray_color(scattered, depth - 1, world);
             return color(0, 0, 0);
         }
 
