ec857b1120b93d579af436c4ee4df84b1dfe9a97
rhit-yangnj <yangnj@rose-hulman.edu>
2024-07-16 11:56:08 -0500

    Completed Book

--- rtweekend.h
+++ rtweekend.h
@@ -17,7 +17,7 @@ const double infinity = std::numeric_limits<double>::infinity();
 const double pi = 3.1415926535897932385;
 
 // Utility Functions
-inline double degrees_to_radian(double degrees) {
+inline double degrees_to_radians(double degrees) {
     return degrees * pi / 180;
 }
 
